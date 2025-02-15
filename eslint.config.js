c80f46f5a7e00454dc6912fce6c8081d0f86dde5
tris <nexus4fortune@gmail.com>
2025-02-15 20:29:41 +0900

    chore: JSX関係のエラーをオフにする設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -10,4 +10,10 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
 ];

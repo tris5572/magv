037bffe99a18de6d5da48c3442ad62f3b3a70d73
tris <nexus4fortune@gmail.com>
2025-03-06 08:09:39 +0900

    feat: インジケーターのクリックによるページ移動を実装

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,5 +1,10 @@
 import { useAtom } from "jotai";
-import { imageListAtom, openingImageIndexAtom } from "../types/zip";
+import {
+  imageListAtom,
+  moveIndexAtom,
+  openingImageIndexAtom,
+} from "../types/zip";
+import { useCallback, useRef } from "react";
 
 /**
  * 画像のページ位置を表示するインジケーターコンポーネント
@@ -7,13 +12,15 @@ import { imageListAtom, openingImageIndexAtom } from "../types/zip";
 export function Indicator() {
   const [list] = useAtom(imageListAtom);
   const [index] = useAtom(openingImageIndexAtom);
+  const [, moveIndex] = useAtom(moveIndexAtom);
+  const elementRef = useRef<HTMLDivElement>(null);
 
   // TODO: 見開き方法を変えられるようになったときは変更に対応する
   const direction = "left";
 
   const last = list.length - 1;
   const width = last === 0 ? 0 : (index / last) * 100;
-  const pageStr = `${index} / ${last}`;
+  const pageStr = last === -1 ? "- / -" : `${index} / ${last}`;
 
   /** 進捗バーのスタイル。Tailwind では動的スタイルを利用できないため、自前でスタイルを生成する */
   const barStyle: React.CSSProperties = {
@@ -26,8 +33,30 @@ export function Indicator() {
     // left: direction === "right" ? 0 : undefined,
   };
 
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      if (elementRef.current) {
+        const clickX = e.clientX;
+        const width = elementRef.current.clientWidth;
+        console.log("handleClick", clickX);
+
+        // TODO: 開き方向を変えられるようになったときは処理を切り替え可能にする
+        const ratio = 1 - clickX / width;
+        const idx = Math.floor(ratio * list.length);
+
+        moveIndex({ index: idx });
+      }
+      e.stopPropagation();
+    },
+    [list.length, moveIndex]
+  );
+
   return (
-    <div className="w-dvw bg-transparent h-8 cursor-pointer relative">
+    <div
+      className="w-dvw bg-transparent h-8 cursor-pointer relative"
+      ref={elementRef}
+      onClick={handleClick}
+    >
       <div style={barStyle}></div>
       <div className="w-dvw h-8 absolute top-0 left-0 flex justify-center items-center text-zinc-200">
         {pageStr}

5568065134b263035519fabe4251de1ec71fcfb8
tris <nexus4fortune@gmail.com>
2025-03-16 06:27:27 +0900

    feat: 見開き表示切替 atom を実装

--- src/atoms/app.ts
+++ src/atoms/app.ts
@@ -18,3 +18,11 @@ export const viewingImageAtom = atom<ViewImageMode | undefined>(undefined);
  * リネーム入力欄を開いているかどうかの atom
  */
 export const isOpeningRenameViewAtom = atom<boolean>(false);
+
+/**
+ * 画像を単体表示にするか見開き表示にするかを切り替える atom
+ *
+ * - "single": 常に1枚のみ表示
+ * - "double": 見開き表示可能な場合(縦画像が2枚連続)は見開き表示
+ */
+export const singleOrDoubleAtom = atom<"single" | "double">("double");

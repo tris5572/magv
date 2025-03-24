e698820fe1343295302b48e9fed3d4705f96f741
tris <nexus4fortune@gmail.com>
2025-03-24 19:54:35 +0900

    feat: インジケーターのドラッグによるページ移動を実装

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useImageData } from "../hooks/images";
 
 /**
@@ -7,6 +7,7 @@ import { useImageData } from "../hooks/images";
 export function Indicator() {
   const imageData = useImageData();
   const elementRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
 
   const index = imageData.index;
   const list = imageData.list;
@@ -30,12 +31,12 @@ export function Indicator() {
     // left: direction === "right" ? 0 : undefined,
   };
 
-  const handleClick = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
+  /** クリックまたはドラッグによるページ移動処理のために呼び出されるコールバック */
+  const handleMoveEvent = useCallback(
+    (e: React.MouseEvent<HTMLDivElement> | MouseEvent) => {
       if (elementRef.current) {
         const clickX = e.clientX;
         const width = elementRef.current.clientWidth;
-        console.log("handleClick", clickX);
 
         // TODO: 開き方向を変えられるようになったときは処理を切り替え可能にする
         const ratio = 1 - clickX / width;
@@ -43,16 +44,60 @@ export function Indicator() {
 
         moveIndex({ index: idx });
       }
-      e.stopPropagation();
     },
     [list.length, moveIndex]
   );
+
+  /** クリック時に呼び出されるコールバック */
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement> | MouseEvent) => {
+      // 左クリックのときだけ移動処理を実行する
+      if (e.button === 0) {
+        handleMoveEvent(e);
+        setIsDragging(true);
+      }
+    },
+    [handleMoveEvent]
+  );
+
+  /** ドラッグ中に呼び出されるコールバック */
+  const handleDrag = useCallback(
+    (e: React.MouseEvent<HTMLDivElement> | MouseEvent) => {
+      if (isDragging) {
+        handleMoveEvent(e);
+      }
+    },
+    [isDragging, handleMoveEvent]
+  );
+
+  // 要素の外（ウィンドウ内）でドラッグされたときにページを移動するためのリスナーを追加
+  useEffect(() => {
+    document.addEventListener("mousemove", handleDrag);
+    return () => {
+      document.removeEventListener("mousemove", handleDrag);
+    };
+  }, [handleDrag]);
+
+  // 要素の外（ウィンドウ内）でマウスアップされたときにドラッグを解除するためのリスナーを追加
+  useEffect(() => {
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+    document.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
 
   return (
     <div
       className="w-dvw bg-transparent h-8 cursor-pointer relative select-none"
       ref={elementRef}
-      onClick={handleClick}
+      onMouseDown={handleClick}
+      onMouseMove={handleDrag}
+      onMouseUp={() => {
+        setIsDragging(false);
+      }}
     >
       <div style={barStyle}></div>
       <div className="w-dvw h-8 absolute top-0 left-0 flex justify-center items-center text-zinc-200">

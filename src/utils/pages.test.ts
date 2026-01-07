12638d8f8f2906d5b7ec28256001e0a1ee48fa82
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-01-08 06:07:46 +0900

    test: モックデータ生成関数を定義

--- src/utils/pages.test.ts
+++ src/utils/pages.test.ts
@@ -1,10 +1,20 @@
 import type { DataSource } from "../types/data";
 import { movePrevSingleImage } from "./pages";
 
-const DEFAULT_DATA_SOURCE: DataSource = {
-  images: [],
+/**
+ * データソースのモックデータを、画像の数から生成する
+ */
+function createMockDataSourceByImageCount(imageCount: number): DataSource {
+  return {
+  images: [...Array(imageCount)].map((i) => ({
+      name: `${i + 1}.png`,
+      source: "",
+      orientation: "portrait",
+    })),
   siblings: [],
 };
+}
+
 
 describe("movePrevSingleImage", () => {
   test("データソースが undefined のとき、undefined を返すこと", () => {
@@ -13,12 +23,18 @@ describe("movePrevSingleImage", () => {
   });
 
   test("インデックスが 0 のとき、undefined を返すこと", () => {
-    const result = movePrevSingleImage({ index: 0, dataSource: DEFAULT_DATA_SOURCE });
+    const result = movePrevSingleImage({
+      index: 0,
+      dataSource: createMockDataSourceByImageCount(3),
+    });
     expect(result).toBeUndefined();
   });
 
   test("インデックスが 1 のとき、0 を返すこと", () => {
-    const result = movePrevSingleImage({ index: 1, dataSource: DEFAULT_DATA_SOURCE });
+    const result = movePrevSingleImage({
+      index: 1,
+      dataSource: createMockDataSourceByImageCount(3),
+    });
     expect(result).toBe(0);
   });
 });
